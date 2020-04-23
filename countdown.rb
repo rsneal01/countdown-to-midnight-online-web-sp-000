14a633880681fd1bea6e41ac60b7c5e3a8c3f5cc
Bobby Neal <rsneal01@gmail.com>
2020-04-23 16:22:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
 #write your code here
 
 def countdown(number)
-  while number > 0
+  while number.to_i > 0
   number -= 1
     puts "#{number} SECOND(S)!"
   end
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep()
+def countdown_with_sleep(number)
+  
